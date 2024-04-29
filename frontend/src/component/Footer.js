975fc214d787a9f4e1e651d6efc3b7e5f59e7f45
Gautam Kumar <kumargautamsp@gmail.com>
2024-04-29 23:26:41 +0530

    Full Project

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+  return (
+    <div className={styles.fText}>
+        <p>© Copyright 2024 | All right reserved by Bhanu Prakash</p>
+    </div>
+  )
+}
+export default Footer
